e98aab85ee1eb3b84826792112de74ec4d458ab8
Postbird <ptbird@yeah.net>
2019-08-28 01:42:17 +0800

    feat: ios

--- demo1/lib/main.dart
+++ demo1/lib/main.dart
@@ -1,4 +1,11 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+
+const IMAGE_SRC =
+    'http://imglf3.nosdn.127.net/img/Mm9KQTVTN2NLSmxOdXp0Q3pRMTYycm1IakVPcERLOTNPVjRTcEJrZ2M5ZUpuMk1WMXJGNEhBPT0.jpg';
 
 void main() {
   runApp(MyApp());
@@ -11,133 +18,129 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('渐变'),
-        ),
-        body: HomeContent(),
-      ),
+          appBar: AppBar(
+            title: Text('文件读写 Demo'),
+          ),
+          body: HomeDemo()),
     );
   }
 }
 
-class HomeContent extends StatelessWidget {
-  const HomeContent({Key key}) : super(key: key);
-
+class HomeDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-      // 径向渐变
-      Container(
-        width: 200,
-        height: 100,
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            // Alignment(x,y)  (0,0) 是由 rectangle 中心点计算的
-            begin: Alignment(0.0, -1.0),
-            end: Alignment(0.0, 1.0),
-            colors: <Color>[
-              Color(0xFFFF5000),
-              Color(0xFFFF9000),
-              Colors.black,
-            ],
-          ),
-        ),
-      ),
-          SizedBox(height: 10),
-          // 横向渐变
-Container(
-  width: 200,
-  height: 50,
-  decoration: BoxDecoration(
-    gradient: LinearGradient(
-      begin: Alignment(-1, 0),
-      end: Alignment(1.0, 0),
-      colors: <Color>[
-        const Color(0xFFFF5000),
-        const Color(0xFFFF9000),
-      ],
-    ),
-  ),
-),
-          SizedBox(height: 10),
-          FollowWidget(),
-        ],
+      child: OutlineButton(
+        child: Text('go to counter'),
+        onPressed: () => {
+          Navigator.of(context).push(MaterialPageRoute(builder:(context) => CounterDemo()))
+        },
       ),
     );
   }
 }
 
-class FollowWidget extends StatefulWidget {
-  FollowWidget({Key key}) : super(key: key);
-
-  _FollowWidgetState createState() => _FollowWidgetState();
+class CounterDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('counter')
+      ),
+      body: FileIODemo()
+    );
+  }
 }
 
-class _FollowWidgetState extends State<FollowWidget> {
-  bool _followStatus = false;
 
-  void _followClickHandle() {
+class FileIODemo extends StatefulWidget {
+  FileIODemo({Key key}) : super(key: key);
+
+  _FileIODemoState createState() => _FileIODemoState();
+}
+
+class _FileIODemoState extends State<FileIODemo> {
+  int _counter = 0;
+  File _file;
+
+  _FileIODemoState() {
+    _initFile();
+  }
+  @override
+  void initState() {
+    super.initState();
+    _initCounter();
+  }
+
+  _initCounter() async {
+    final File file = await _localFile;
+    final String res = await file.readAsString();
     setState(() {
-      this._followStatus = !this._followStatus;
+      _counter = int.parse(res ?? 0);
     });
   }
 
-  List<Color> _getGradient() {
-    if (this._followStatus) {
-      return <Color>[Colors.grey, Colors.grey];
-    } else {
-      return <Color>[const Color(0xFFFF5000), const Color(0xFFFF9000)];
-    }
+  _initFile() async {
+    _file = await _localFile;
   }
 
-  List<Widget> _getContent() {
-    List<Widget> defaultContent = <Widget>[
-      Text(
-        this._followStatus ? '已关注' : '关注',
-        style: TextStyle(
-          fontSize: 16,
-          color: Colors.white,
-          letterSpacing: 1.2,
-        ),
-      )
-    ];
-    List<Widget> prefixContent = <Widget>[
-      Image.network(
-        'https://gw.alicdn.com/tfs/TB1OC0TXMMPMeJjy1XcXXXpppXa-108-84.png',
-        height: 16,
-      ),
-      SizedBox(width: 3)
-    ];
-    if (!this._followStatus) {
-      defaultContent.insertAll(0, prefixContent);
-    }
-    return defaultContent;
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+    print(directory.path);
+    return directory.path;
   }
+
+  Future<File> get _localFile async {
+    if (_file is File) {
+      return _file;
+    } else {
+      final String path = await _localPath;
+      _file = File('$path/counter.txt');
+      return _file;
+    }
+  }
+  _saveCounter() async {
+    final File file = await _localFile;
+    file.writeAsString(_counter.toString());
+  }
+
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      child: Container(
-        width: 90,
-        height: 40,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(20)),
-          gradient: LinearGradient(
-            begin: Alignment(-1, 0),
-            end: Alignment(1.0, 0),
-            colors: this._getGradient(),
-          ),
-        ),
-        child: Row(
+    return Column(
+      children: <Widget>[
+        SizedBox(height: 20),
+        Row(
           mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: this._getContent(),
+          children: <Widget>[
+            Text(_counter.toString()),
+          ],
         ),
-      ),
-      onTap: this._followClickHandle,
+        SizedBox(height: 20),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            RaisedButton(
+              child: Text('-'),
+              onPressed: () {
+                setState(() {
+                  _counter -= 1;
+                  _saveCounter();
+                });
+              },
+            ),
+            RaisedButton(
+              child: Text('+'),
+              onPressed: () {
+                setState(() {
+                  _counter += 1;
+                  _saveCounter();
+                });
+              },
+            ),
+          ],
+        )
+      ],
     );
   }
 }
